1c575ac77526a1f86a131dc7812a9c3168c4ec74
Dennis Tel <dennistel90@gmail.com>
2013-03-06 15:34:54 +0100

    shit werkend

--- meteor_chat_app.js
+++ meteor_chat_app.js
@@ -1,13 +1,37 @@
+var Messages = new Meteor.Collection("messages")
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to meteor_chat_app.";
+  Template.messages.messages = function(){
+    return Messages.find({}, {sort:{time: -1}})
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+
+  Template.chatfields.events({
+    "click .button" : function(){
+      
+      var latitude, longitude;
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(show_pos)
+      }
+      function show_pos(position){
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+
+        var $messageBox = $("#messageContent")
+        var messageContent = $messageBox.val()
+        var name = Meteor.user().profile.name
+        $messageBox.val("");
+
+        saveMessage(latitude, longitude, name, messageContent);
+      }
+      
+      function saveMessage(latitude, longitude, name, messageContent){
+        // console.log(messageContent)
+        // TODO: veilig maken op de server
+        var time = new Date();
+        Messages.insert({"body" : messageContent, "name" : name, "latitude": latitude, "longitude": longitude, "time": time})
+      }
+      
     }
   });
 }
@@ -16,4 +40,5 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
 }
